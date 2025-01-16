4a859124bdadfbc09a2c60a0db2addf16d55302e
Bjarke11 <Bjarke.sp.1111@gmail.com>
2025-01-16 08:40:38 +0100

    feat: changed server lobby requests to petrinet model

--- tank_game_project_02148/src/main/java/dk/dtu/connection/Server.java
+++ tank_game_project_02148/src/main/java/dk/dtu/connection/Server.java
@@ -1,4 +1,4 @@
-package dk.dtu.connection;
+package dk.dtu;
 
 import java.io.IOException;
 import java.net.UnknownHostException;
@@ -6,32 +6,100 @@ import java.net.UnknownHostException;
 import org.jspace.*;
 
 public class Server {
+    public static Space p0 = new SequentialSpace();
+    public static Space p1 = new SequentialSpace();
+    public static Space p2 = new SequentialSpace();
+    public static Space p3 = new SequentialSpace();
+    public static Space p4 = new SequentialSpace();
+    public static SpaceRepository serverSpace;
+    public static RemoteSpace lobbyRequests;
+    final static String IP = "10.209.247.105";
+    final static String port = "31145";
+    public static int lobbyID;
+
     public static void main(String[] args) throws UnknownHostException, IOException {
-        final String IP = "localhost";
-        final String port = "31145";
-        int lobbyID = 0;
-        SpaceRepository serverSpace = new SpaceRepository();
+        lobbyID = 0; 
+        serverSpace = new SpaceRepository();
         serverSpace.addGate("tcp://" + IP + ":" + port + "/?conn");
         serverSpace.add("lobbyRequests", new SequentialSpace());
         System.out.println("server up");
+        
+//START PETRINET
 
-        RemoteSpace lobbyRequests = new RemoteSpace("tcp://" + IP + ":" + port + "/lobbyRequests?conn");
+        new Thread(() -> connectToRequests()).start();
+        new Thread(() -> lobbyReq()).start();
+        new Thread(() -> createLobbyHandler()).start();
+        new Thread(() -> sendLobbyID()).start();
+        new Thread(() -> increaseLobbyID()).start();  
+        try {
+            p0.put("token");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }    
+    }
 
+    public static void connectToRequests() {
         while (true) {
             try {
+                p0.get(new ActualField("token"));
+                lobbyRequests = new RemoteSpace("tcp://" + IP + ":" + port + "/lobbyRequests?conn");
+                p1.put("token");
+            } catch (InterruptedException | IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void lobbyReq() {
+        while (true) {
+            try {
+                p1.get(new ActualField("token"));
                 lobbyRequests.get(new ActualField("host"));
-                new Thread(new lobbyHandeler(IP, port, lobbyID, serverSpace)).start();
-                System.out.println("lobby: " + (lobbyID) + " started");
-                lobbyRequests.put("lobby", lobbyID);
-                lobbyID++;
+                p2.put("token");
+                p3.put("token");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+    }
 
+    public static void createLobbyHandler() {
+        while (true) {
+            try {
+                p2.get(new ActualField("token"));
+                new Thread(new lobbyHandeler(IP, port, lobbyID, serverSpace)).start();
+                System.out.println("lobby: " + (lobbyID) + " started");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void sendLobbyID() {
+        while (true) {
+            try {
+                p3.get(new ActualField("token"));
+                lobbyRequests.put("lobby", lobbyID);
+                p4.put("token");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void increaseLobbyID() {
+        while (true) {
+            try {
+                p4.get(new ActualField("token"));
+                lobbyID++;
+                p1.put("token");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
-
+//END PETRINET
 class lobbyHandeler implements Runnable {
     private String IP;
     private String port;
@@ -55,7 +123,7 @@ class lobbyHandeler implements Runnable {
         try {
             Space lobbyShots = new RemoteSpace(uri);
             lobbyShots.query(new ActualField("Game Over"), new FormalField(String.class));
-            Thread.sleep(1000);
+            Thread.sleep(200);
             serverSpace.remove(lobbyID + "player1");
             serverSpace.remove(lobbyID + "player2");
             serverSpace.remove(lobbyID + "shots");
@@ -63,7 +131,5 @@ class lobbyHandeler implements Runnable {
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
-        
-       
     }
 }
